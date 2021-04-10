d3c963aeb485101547776af163c1c7830d001de5
Joshua Lisoski <56094425+JLisoski@users.noreply.github.com>
2021-04-10 07:48:56 -0700

    Added Set Number of Node Functionality and setup main output box.

--- CS489HW7/CS489HW7/Form1.Designer.cs
+++ CS489HW7/CS489HW7/Form1.Designer.cs
@@ -29,21 +29,119 @@ namespace CS489HW7
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Set Number of Input Nodes:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Set Number of Hidden Nodes:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(9, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Set Number of Output Nodes:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(9, 100);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(162, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 39);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Initialize Neural Network";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(187, 7);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(461, 436);
+            this.textBox4.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
---
+++ CS489HW7/CS489HW7/NeuralNetwork.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS489HW7
+{
+    class NeuralNetwork
+    {
+        private int[] layers;
+        public float[][] neurons;
+        public float[][] biases;
+        public float[][][] weights;
+        public string[] inputData;
+        public int numOfLine;
+        //public int[] activations;
+        Random rand = new Random();
+
+        public float fitness = 0;
+
+        public NeuralNetwork(int[] layers)
+        {
+            this.layers = new int[layers.Length];
+
+            for (int i = 0; i < layers.Length; i++)
+            {
+                this.layers[i] = layers[i];
+            }
+
+            InitNeurons();
+            InitBiases();
+            InitWeights();
+        }
+
+        private void InitNeurons()
+        {
+            List<float[]> neuronsList = new List<float[]>();
+
+            for (int i = 0; i < layers.Length; i++)
+            {
+                neuronsList.Add(new float[layers[i]]);
+            }
+
+            neurons = neuronsList.ToArray();
+        }
+
+        private void InitBiases()
+        {
+            List<float[]> biasList = new List<float[]>();
+
+            for (int i = 0; i < layers.Length; i++)
+            {
+                float[] bias = new float[layers[i]];
+
+                for (int j = 0; j < layers[i]; j++)
+                {
+                    //Not sure about this...
+                    bias[j] = (rand.Next() * 1) - 0.5f;
+                }
+                biasList.Add(bias);
+            }
+            biases = biasList.ToArray();
+        }
+
+        private void InitWeights()
+        {
+            List<float[][]> weightsList = new List<float[][]>();
+            for (int i = 1; i < layers.Length; i++)
+            {
+                List<float[]> layerWeightsList = new List<float[]>();
+                int neuronsInPreviousLayer = layers[i - 1];
+                for (int j = 0; j < neurons[i].Length; j++)
+                {
+                    float[] neuronWeights = new float[neuronsInPreviousLayer];
+                    for (int k = 0; k < neuronsInPreviousLayer; k++)
+                    {
+                        neuronWeights[k] = (rand.Next() * 1) - 0.5f;
+                    }
+                    layerWeightsList.Add(neuronWeights);
+                }
+                weightsList.Add(layerWeightsList.ToArray());
+            }
+            weights = weightsList.ToArray();
+        }
+
+        public float activate(float value)
+        {
+            return (float)Math.Tanh(value);
+        }
+
+        public float[] FeedForward(float[] inputs)
+        {
+            //For Debugging
+            //Console.WriteLine("Neurons Length = " + neurons.Length.ToString());
+            //Console.WriteLine("Inputs Length = " + inputs.Length.ToString());
+
+            for (int i = 0; i < neurons.Length; i++)
+            {
+                for (int j = 0; j < inputs.Length; j++)
+                {
+                    if (j < neurons[i].Length)
+                    {
+                        //Console.WriteLine("Neurons Length = " + neurons[i].Length.ToString());
+                        //Console.WriteLine("i = " + i.ToString());
+                        //Console.WriteLine("j = " + j.ToString());
+                        neurons[i][j] = inputs[j];
+                    }
+                }
+            }
+
+            for (int i = 1; i < layers.Length; i++)
+            {
+                int layer = i - 1;
+                for (int j = 0; j < neurons[i].Length; j++)
+                {
+                    float value = 0f;
+                    for (int k = 0; k < neurons[i - 1].Length; k++)
+                    {
+                        value += weights[i - 1][j][k] * neurons[i - 1][k];
+                    }
+                    neurons[i][j] = activate(value + biases[i][j]);
+                }
+            }
+            return neurons[neurons.Length - 1];
+        }
+
+        public int CompareNetworks(NeuralNetwork other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            else if (fitness > other.fitness)
+            {
+                return 1;
+            }else if(fitness < other.fitness)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public void Load(string path)
+        {
+            string temporary = "";
+            int counter = 0;
+            string[] temp = new string[100];
+            inputData = new string[100];
+            int id = 0;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        temporary = sr.ReadLine();
+                        temp[id] = temporary;
+                        //inputData[id] = temporary;
+                        //For Debugging
+                        Console.WriteLine(temporary);
+                        inputData[id] = temporary;
+
+                        id++;
+                        counter++;
+                    }
+                    sr.Close();
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read: ");
+                Console.WriteLine(e.Message);
+            }
+
+            //Grab line count
+            numOfLine = counter;
+            //For Debugging
+            //Console.WriteLine("The number of lines are... " + numOfLine.ToString());
+
+            string[] ListLines = new string[numOfLine];
+            int index = 1;
+
+            for (int i = 0; i < numOfLine; i++)
+            {
+                ListLines[i] = temp[i];
+
+                //For Debugging
+                //Console.WriteLine("ListLines[] = " + ListLines[i].ToString());
+            }
+
+            //For Debugging
+            Console.WriteLine("Being Reading File...");
+
+            if (new FileInfo(path).Length > 0)
+            {
+                //For Debugging
+                //Console.WriteLine("Inside the if path is longer then zero...");
+
+                for (int i = 0; i < biases.Length; i++)
+                {
+                    for (int j = 0; j < biases[i].Length; j++)
+                    {
+                        biases[i][j] = float.Parse(ListLines[index]);
+                        index++;
+                    }
+                }
+
+                //For Debugging
+                //Console.WriteLine("Index Before Second For Loop = " + index.ToString());
+
+                for (int i = 0; i < weights.Length; i++)
+                {
+                    //Console.WriteLine("weights.length = " + weights.Length.ToString());
+                    for (int j = 0; j < weights[i].Length; j++)
+                    {
+                        //Console.WriteLine("weights[i].length = " + weights[i].Length.ToString());
+                        for (int k = 0; k < weights[i][j].Length; k++)
+                        {
+                            //Console.WriteLine("weights[i][j].length = " + weights[i][j].Length.ToString());
+                            //Console.WriteLine("i = " + i.ToString() + ", j = " + j.ToString() + ", k = " + k.ToString());
+                            //Console.WriteLine("ListLines Length = " + ListLines.Length);
+                            //Console.WriteLine("Index = " + index.ToString());
+
+                            if(index < 16)
+                            {
+                                weights[i][j][k] = float.Parse(ListLines[index]);
+                            }
+
+                            index++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("File Read!!!\r\n");
+        }
+
+        public void Mutate(int chance, float val)
+        {
+            for (int i = 0; i < biases.Length; i++)
+            {
+                for (int j = 0; j < biases[i].Length; j++)
+                {
+                    biases[i][j] = ((rand.Next()*(chance)) <= 5) ? biases[i][j] += (rand.Next()*(val-(-val)))+(val) : biases[i][j];
+                }
+            }
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                for (int j = 0; j < weights[i].Length; j++)
+                {
+                    for (int k = 0; k < weights[i][j].Length; k++)
+                    {
+                        weights[i][j][k] = ((rand.Next() * (chance)) <= 5) ? weights[i][j][k] += (rand.Next() * (val - (-val))) + (val) : weights[i][j][k];
+
+                    }
+                }
+            }
+        }
+
+
+    }
+}
